6100c4a939cb8c3b2a7d07144f10b928804e696b
zscaiosi <zscaio.si@gmail.com>
2018-01-28 19:34:01 -0200

    []

--- consensus-website/src/App.tsx
+++ consensus-website/src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import './App.css';
 
-const logo = require('./logo.svg');
+const logo = require('./volunteer.svg');
 
 class App extends React.Component {
   render() {
@@ -9,11 +9,31 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">CONSENSUS</h1>
+          <h4 className="sub-title">Uma nova forma de assembléia</h4>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <section className="centered">
+          <article className="description">
+            <p className="p-text">
+              Consensus é um projeto <i> <a target="_blank" href="https://opensource.org/about"> Open Source </a></i> licenciado sob a <i title="GNU"> <a target="_blank" href="https://www.gnu.org/licenses/gpl-3.0.pt-br.html">GNU General Public License</a> </i> destinado às pessoas que possam e queiram contribuir para uma nova forma de gerar debates, assembléias, reuniões e deliberações.  
+            </p>
+            <p className="p-text">
+              Uma das maiores dificuldades em realizar assembléias de pessoas para deliberar ou debater determinado assunto é o esforço logístico que deve ser empreendido, quanto maior o número de pessoas, maior é esse esforço. <br/>
+              Muitas vezes não só o esforço logístico é necessário, mas também um esforço de organização e gestão de conflitos durante a assembléia, pois frequentemente os participantes não respeitam o tempo de fala dos outros e/ou não conseguem manter suas falas conectadas com a pauta proposta.
+            </p>
+          </article>
+        </section>
+        <section className="centered">
+          <p className="p-text">
+            A plataforma pretende permitir que um usuário crie uma sala, envie o link para os participantes e então configure as regras da assembléia como quiser; os participantes podem, durante seu tempo de fala, expor suas argumentações a respeito de cada tópico da pauta.<br/>
+            Por fim, os participantes votam nas falas mais relevantes e então pode-se ter uma deliberação a partir dos resultados das argumentações.
+          </p>
+        </section>
+        <section className="centered">
+          <p className="p-text">
+            A plataforma será composta por dois subprojetos, o primeiro é o desenvolvimento do <i>front end</i>, o segundo é o desenvolvimento do back end. Ambos serão desenvolvidos em JavaScript usando ReactJS para o primeiro e NodeJS para o segundo.
+          </p>
+        </section>
       </div>
     );
   }
