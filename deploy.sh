d7c8c8180599a5d1e1ddbed1b983924eff850998
zscaiosi <zscaio.si@gmail.com>
2018-01-28 18:33:40 -0200

    [Deploy] Usando_Bash

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 TAG=$1
 DESCRIPTION=$2
 rsync -a -v consensus-website/build/* .
-#mv consensus-website/build/* .
 git add .
-git commit -a -m '[$TAG] $DESCRIPTION'
+git commit -a -m "[$TAG] $DESCRIPTION"
